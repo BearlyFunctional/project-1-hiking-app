8d7bb6f2dccf29034f2ac928a2f7e10c0d9ba87e
Rob Gould <rgould.midwest@gmail.com>
2023-10-27 00:37:27 -0500

    modal

--- Assets/script.js
+++ Assets/script.js
@@ -77,15 +77,20 @@ $(document).ready(function () {
 				console.error("Error fetching location data:", error);
 			});
 	}
+ // Initialize Materialize modals
+
+
 	// Add event listener to the button
 	document.getElementById("getSearch").addEventListener("click", getWeather);
+	
+	
+
 
 	var map = L.map("map").setView([51.505, -0.09], 13);
-
 	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		maxZoom: 19,
 		attribution:
 			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	}).addTo(map);
+	
 });
- 
