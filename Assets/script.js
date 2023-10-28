2e150ae34adff5bb4b6a16ee69c02d8b7a54a291
Rob Gould <rgould.midwest@gmail.com>
2023-10-28 01:56:14 -0500

    added weather map and 2 overlays

--- Assets/script.js
+++ Assets/script.js
@@ -19,6 +19,8 @@ $(document).ready(function () {
 	updateCurrentTime();
 	// update the current date and time every second
 	setInterval(updateCurrentTime, 1000);
+	
+	// Function to fetch and display weather data
 	function getWeather() {
 		const weatherApiKey = "661e7eff94c386fb32110da5f695f39b";
 		const location = document.getElementById("locationInput").value;
@@ -65,24 +67,33 @@ $(document).ready(function () {
 
 									weatherIcon.src = `${baseIconUrl}${icon}.png`;
 									tempDay.textContent = `${temp}° Fahrenheit`;
-									
+
 									// Convert the dt_txt string to a Date object and format it
 									const date = new Date(dt_txt);
-									const options = { weekday: 'short', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
-									const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-									
+									const options = {
+										weekday: "short",
+										month: "short",
+										day: "numeric",
+										hour: "2-digit",
+										minute: "2-digit",
+										hour12: true,
+									};
+									const formattedDate = new Intl.DateTimeFormat(
+										"en-US",
+										options
+									).format(date);
+
 									dateTime.textContent = formattedDate;
 
 									weatherDayDiv.appendChild(dateTime);
 									weatherDayDiv.appendChild(weatherIcon);
 									weatherDayDiv.appendChild(tempDay);
 
-									weatherDataDiv.appendChild(weatherDayDiv);// Append to modal
+									weatherDataDiv.appendChild(weatherDayDiv); // Append to modal
 								}
 							});
 							// Select a second location to display the search results with the same ID "weatherData"
-							const secondWeatherDataDiv =
-								document.getElementById("weatherData");
+							const secondWeatherDataDiv = document.getElementById("weatherData");
 
 							// Clear the second location's previous results before adding new ones
 							secondWeatherDataDiv.innerHTML = "";
@@ -100,11 +111,21 @@ $(document).ready(function () {
 
 									weatherIcon.src = `${baseIconUrl}${icon}@2x.png`;
 									tempDay.textContent = `${temp}° Fahrenheit`;
-									
+
 									const date = new Date(dt_txt);
-									const options = { weekday: 'short', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
-									const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-							
+									const options = {
+										weekday: "short",
+										month: "short",
+										day: "numeric",
+										hour: "2-digit",
+										minute: "2-digit",
+										hour12: true,
+									};
+									const formattedDate = new Intl.DateTimeFormat(
+										"en-US",
+										options
+									).format(date);
+
 									dateTime.textContent = formattedDate;
 
 									weatherDayDiv.appendChild(dateTime);
@@ -128,16 +149,34 @@ $(document).ready(function () {
 			.catch((error) => {
 				console.error("Error fetching location data:", error);
 			});
-	}
+		
+		}
 	// Add event listener to the button
-	document.getElementById("getSearch").addEventListener("click", getWeather);
+	document.getElementById("getSearch").addEventListener("click", function() {
+		// Call the getWeather function
+		getWeather();
+	
+		// Show the weather map
+		const weatherMapDiv = document.getElementById("weatherMap");
+		weatherMapDiv.style.display = "block";
+	
+		var map = L.map('weatherMap').setView([37.0902, -95.7129], 4);
+	
+		// Create the OpenStreetMap layer
+		var openStreetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom:10,
+			attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+	
+		// Add the precipitation overlay
+		L.imageOverlay('https://tile.openweathermap.org/map/precipitation_new/0/0/0.png?appid=661e7eff94c386fb32110da5f695f39b', 
+			[[85.0, -180], [-85.0, 180]], { opacity: 1.0 }
+		).addTo(map);
 
-	var map = L.map("map").setView([51.505, -0.09], 13);
+// Add the precipitation overlay
+L.imageOverlay('https://tile.openweathermap.org/map/temp_new/0/0/0.png?appid=661e7eff94c386fb32110da5f695f39b', 
+[[85.0, -180], [-85.0, 180]], { opacity: 0.5 }
+).addTo(map);
 
-	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-		maxZoom: 19,
-		attribution:
-			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	}).addTo(map);
+	});
 });
- 
