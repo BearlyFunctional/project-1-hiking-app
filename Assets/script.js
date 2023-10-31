bc0eae0e38671368fd74ba34ca9e60b8143807a3
Rob Gould <rgould.midwest@gmail.com>
2023-10-30 20:30:07 -0500

    updating style, html, id names

--- Assets/script.js
+++ Assets/script.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 		const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${weatherApiKey}`;
 		const iconCode = "10d"; //temporary iconCode
 		const baseIconUrl = "https://openweathermap.org/img/wn/"; //baseURL for icon
-		const iconUrl = `${baseIconUrl}${iconCode}@2x.png`; //structure to call icons
+		//const iconUrl = `${baseIconUrl}${iconCode}@2x.png`; //structure to call icons
 		// Make an API request to get location coordinates with geoURL in order to use City, State, Country
 		fetch(geoUrl)
 			.then((response) => response.json())
@@ -46,8 +46,7 @@ $(document).ready(function () {
 					fetch(weatherUrl)
 						.then((response) => response.json())
 						.then((data) => {
-							// Extract and display weather data by the hour
-							//structure to call icons
+							// Extract and display weather data
 							const weatherData = data.list;
 							console.log(weatherData);
 
@@ -55,27 +54,35 @@ $(document).ready(function () {
 							// Update the "weatherData" div with the data
 							const itemsToShow = 9;
 							const itemsToShow2 = 40;
-							const weatherDiv = document.getElementById("weatherData");
-							const modal = document.getElementById("myModal");
+							//const weatherDiv = document.getElementById("weatherData");
+							const modal = document.getElementById("weatherModal");
 							const weatherDataDiv = modal.querySelector("#weatherData");
 							weatherDataDiv.innerHTML = "";
 
-							weatherData.forEach(function ({ dt_txt, main, weather }, index) {
+							weatherData.forEach(function ({ dt_txt, main, weather, wind }, index) {
 								if (index % itemsToShow === 0) {
 									const { temp } = main;
 									const { icon } = weather[0];
 									const { description } = weather[0];
+									const { humidity } = main;
+									const { speed } = wind;
+									
 
 									const weatherDayDiv = document.createElement("div");
 									const dateTime = document.createElement("p");
 									const tempDay = document.createElement("p");
 									const weatherIcon = document.createElement("img");
 									const weatherDesc = document.createElement("p")
+									const humidDay = document.createElement("p");
+									const windSpeed = document.createElement("p")
+									
 
 									weatherIcon.src = `${baseIconUrl}${icon}.png`;
-									tempDay.textContent = `${temp}° Fahrenheit`;
+									tempDay.textContent = `${temp}° F`;
+									humidDay.textContent = `Humidity: ${humidity}%`;
+									windSpeed.textContent = `Wind: ${speed} mph`;
 									weatherDesc.textContent = `${description}`;
-
+									
 									// Convert the dt_txt string to a Date object and format it
 									const date = new Date(dt_txt);
 									const options = {
@@ -93,47 +100,56 @@ $(document).ready(function () {
 
 									weatherDayDiv.appendChild(dateTime);
 									weatherDayDiv.appendChild(weatherIcon);
-									weatherDayDiv.appendChild(weatherDesc)
+									weatherDayDiv.appendChild(weatherDesc);
 									weatherDayDiv.appendChild(tempDay);
-
+									weatherDayDiv.appendChild(humidDay);
+									weatherDayDiv.appendChild(windSpeed);
 									weatherDataDiv.appendChild(weatherDayDiv); // Append to modal
 								}
 							});
-							// Select a second location to display the search results with the same ID "weatherData"
+							//display the search results with the same ID "weatherData on main page"
 							const secondWeatherDataDiv =
 								document.getElementById("weatherData");
 
 							// Clear the second location's previous results before adding new ones
 							secondWeatherDataDiv.innerHTML = "";
 
-							// Append the elements to the second location's weatherData div
-							weatherData.forEach(function ({ dt_txt, main, weather }, index) {
+							// Append the elements to main page weatherData div
+							weatherData.forEach(function ({ dt_txt, main, weather, wind, }, index) {
 								if (index % itemsToShow2 === 0) {
 									const { temp } = main;
 									const { icon } = weather[0];
+									const { description } = weather[0];
 									const { humidity } = main;
-
+									const { speed } = wind;
+									
+									//create elements
 									const weatherDayDiv = document.createElement("div");
 									const dateTime = document.createElement("p");
 									const tempDay = document.createElement("p");
 									const tempDayC = document.createElement("p");
 									const humidDay = document.createElement("p");
+									const windSpeed = document.createElement("p")
 									const weatherIcon = document.createElement("img");
+									const weatherDesc = document.createElement("p")
 
 									//convert temp from F to C
 									function fahrenheitToCelcius(fahrenheit){
 										return (fahrenheit - 32) * (5/9);
 									}
-
+									//declare temp conversion variables
 									const tempFahrenheit = temp;
 									const tempCelcius = fahrenheitToCelcius(tempFahrenheit);
 
-
+									//content from array to append
 									weatherIcon.src = `${baseIconUrl}${icon}@2x.png`;
-									tempDay.textContent = `${tempFahrenheit}° Fahrenheit`;
-									tempDayC.textContent = `${tempCelcius.toFixed(2)}° Celcius`;
+									tempDay.textContent = `${tempFahrenheit}° F /  ${tempCelcius.toFixed(2)}° C`;
+									//tempDayC.textContent = `${tempCelcius.toFixed(2)}° C`;
 									humidDay.textContent = `Humidity: ${humidity}%`;
+									windSpeed.textContent = `Wind: ${speed} mph`;
+									weatherDesc.textContent = `${description}`;
 
+									//date parameters to display
 									const date = new Date(dt_txt);
 									const options = {
 										weekday: "short",
@@ -143,20 +159,23 @@ $(document).ready(function () {
 										//minute: "2-digit",
 										hour12: true,
 									};
+
 									const formattedDate = new Intl.DateTimeFormat(
 										"en-US",
 										options
 									).format(date);
-
-									
-
+									//format date
 									dateTime.textContent = formattedDate;
 
-									weatherDayDiv.appendChild(dateTime);
+									//append elements in order of preference
+									//weatherDayDiv.appendChild(dateTime);
 									weatherDayDiv.appendChild(weatherIcon);
+									weatherDayDiv.appendChild(weatherDesc);
 									weatherDayDiv.appendChild(humidDay);
+									weatherDayDiv.appendChild(windSpeed);
 									weatherDayDiv.appendChild(tempDay);
 									weatherDayDiv.appendChild(tempDayC);
+									
 
 									secondWeatherDataDiv.appendChild(weatherDayDiv); // Append to the second location's weatherData div
 									updateMap(lat, lon);
@@ -182,6 +201,7 @@ $(document).ready(function () {
 	function updateMap(lat, lon) {
 		if (!map) {
 			// Create the map and layers from OpenWeather
+			// base layer/map
 			map = L.map("weatherMap").setView([lat, lon], 10);
 			openStreetMapLayer = L.tileLayer(
 				"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -191,6 +211,7 @@ $(document).ready(function () {
 						'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 				}
 			).addTo(map);
+			//weather layer precipitation
 			const precipitationLayer = L.tileLayer(
 				"https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=661e7eff94c386fb32110da5f695f39b",
 				{
@@ -198,7 +219,7 @@ $(document).ready(function () {
 					opacity: 1.0,
 				}
 			);
-
+			//weather layer clouds
 			const cloudLayer = L.tileLayer(
 				"https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=661e7eff94c386fb32110da5f695f39b",
 				{
@@ -206,7 +227,7 @@ $(document).ready(function () {
 					opacity: 1.0,
 				}
 			);
-
+			//weather layer temperature/heatmap
 			const temperatureLayer = L.tileLayer(
 				"https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=661e7eff94c386fb32110da5f695f39b",
 				{
@@ -214,7 +235,7 @@ $(document).ready(function () {
 					opacity: 0.3,
 				}
 			);
-
+			//weather layer wind
 			const windLayer = L.tileLayer(
 				"https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=661e7eff94c386fb32110da5f695f39b",
 				{
@@ -222,7 +243,7 @@ $(document).ready(function () {
 					opacity: 1.0,
 				}
 			);
-
+			//weather layer pressure
 			const pressureLayer = L.tileLayer(
 				"https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=661e7eff94c386fb32110da5f695f39b",
 				{
@@ -230,7 +251,8 @@ $(document).ready(function () {
 					opacity: 0.5,
 				}
 			);
-
+			
+			//declare variables to display layer options 
 			const weatherLayers = L.layerGroup([
 				precipitationLayer,
 				cloudLayer,
@@ -261,14 +283,14 @@ $(document).ready(function () {
 				// Add more overlay layers as needed
 			};
 
-			L.control.layers(baseLayers, overlayLayers).addTo(map);
+			L.control.layers(baseLayers, overlayLayers, { collapsed:false }).addTo(map);
 
 			map.on("baselayerchange", function (eventLayer) {
 				const selectedLater = eventLayer.name;
 				console.log("Selected Later: ${selectedLayer");
 			});
 
-			// Create a draggable marker with the custom icon and add it to the map.
+			// Create a draggable marker and add it to the map.
 			marker = L.marker([lat, lon], {
 				draggable: true, // Make the marker draggable
 			}).addTo(map);
@@ -288,10 +310,10 @@ $(document).ready(function () {
 			// Update the map's view.
 			map.setView([lat, lon]);
 
-			// Update the marker's position.
+			// Update the marker's position when moved
 			marker.setLatLng([lat, lon]);
 
-			// Update the marker's tooltip content.
+			// Update the marker's tooltip content when moved
 			var newLatLng = marker.getLatLng();
 			marker
 				.getTooltip()
@@ -300,7 +322,7 @@ $(document).ready(function () {
 		}
 	}
 
-	// Add an event listener to the "Search" button
+	// Add an event listener to the "Search" button to fire the search
 	document.getElementById("getSearch").addEventListener("click", function () {
 		// Call the getWeather function
 		getWeather();
@@ -309,33 +331,21 @@ $(document).ready(function () {
 		const weatherMapDiv = document.getElementById("weatherMap");
 		weatherMapDiv.style.display = "block";
 
+		//show the 5 day forecast modal button
 		const forecastbtn = document.getElementById("forecastbtn");
 		forecastbtn.style.display = "block";
 
+		//show the weatherdata
 		const wdata = document.getElementById("weatherData");
 		wdata.style.display = "block";
+
+		// Get the input value from #locationInput
+		const locationInput = document.getElementById("locationInput").value;
+    
+		// Get the "locationSearched" div
+		const locationSearchedDiv = document.querySelector(".locationSearched");
+
+		// Update the content of the "locationSearched" div
+		locationSearchedDiv.innerHTML = `${locationInput}`;
 	});
 });
-
-// Show the weather map
-// const weatherMapDiv = document.getElementById("weatherMap");
-// weatherMapDiv.style.display = "block";
-//});
-//});
-//	var map = L.map('weatherMap').setView([37.0902, -95.7129], 4);
-
-// Create the OpenStreetMap layer
-//	var openStreetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//		maxZoom:10,
-//		attribution: 'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//	}).addTo(map);
-
-// Add the precipitation overlay
-//	L.imageOverlay('https://tile.openweathermap.org/map/precipitation_new/0/0/0.png?appid=661e7eff94c386fb32110da5f695f39b',
-//		[[85.0, -180], [-85.0, 180]], { opacity: 1.0 }
-//	).addTo(map);
-
-// Add the precipitation overlay
-//L.imageOverlay('https://tile.openweathermap.org/map/temp_new/0/0/0.png?appid=661e7eff94c386fb32110da5f695f39b',
-//[[85.0, -180], [-85.0, 180]], { opacity: 0.5 }
-//).addTo(map);
